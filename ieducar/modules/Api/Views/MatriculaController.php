52e4cc738c686e0d3cde13c2e769c45c44e1c98c
Robert Ferraz <robert@portabilis.com.br>
2025-02-24 21:33:55 -0300

    Remove valor nulo ao desfazer deixou de frequentar

--- ieducar/modules/Api/Views/MatriculaController.php
+++ ieducar/modules/Api/Views/MatriculaController.php
@@ -521,8 +521,7 @@ class MatriculaController extends ApiCoreController
             $sql = 'UPDATE pmieducar.matricula
               SET aprovado = $1,
                   ref_cod_abandono_tipo = $2,
-                  data_exclusao = NULL,
-                  deixou_de_frequentar_idade_obrigatoria = NULL
+                  data_exclusao = NULL
               WHERE cod_matricula = $3';
             $this->fetchPreparedQuery($sql, [$situacaoAndamento, $tipoSemAbandono, $matriculaId]);
 
