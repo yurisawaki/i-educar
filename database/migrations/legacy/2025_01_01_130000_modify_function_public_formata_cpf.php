d4a7b36be8e24ace9481ae03772f84ebd796b274
Marcos Oliveira Soares <marcosoliveirasoares94@gmail.com>
2025-05-25 23:57:56 -0300

    Corrigindo nome interno da classe.

--- database/migrations/legacy/2025_01_01_130000_modify_function_public_formata_cpf.php
+++ database/migrations/legacy/2025_01_01_130000_modify_function_public_formata_cpf.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 
-class AddFunctionPublicFormataCpf extends Migration
+class ModifyFunctionPublicFormataCpf extends Migration
 {
     /**
      * Run the migrations.
